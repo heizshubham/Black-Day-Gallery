54e7bf46f77f924caed1b4c5dd9f8ef80c430ace
heizshubham <shubhamp01947@gmail.com>
2024-02-13 01:06:19 +0530

    new

--- script.js
+++ script.js
@@ -13,5 +13,23 @@ document.addEventListener('click',activate,false);
 
 document.addEventListener('DOMContentLoaded', function () {
   // This function will be executed when the DOM is fully loaded
-  alert('Hi there this is Shubham Pandey and Welcome to the website, some days ago i went to tha HAL museum Bengaluru and there I explored many things Liike Aircraft,FighterJet etc.. and I clicked some photos of Aircraft/FighterJet and from that I made a website to spread the knowledge about HAL and their Aircrafts. "Thanks for visiting".');
+  alert('Hi there this is Shubham Pandey (instagram page @slaycount) Follow me on Instagram if you like this web page project, "Thanks for visiting".');
 });
+
+// JavaScript code for screen compatibility
+function checkScreenSize() {
+  // Get the width of the screen
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  // Log a message based on screen width
+  if (screenWidth < 600) {
+    console.log("Small screen: " + screenWidth + "px width");
+  } else if (screenWidth < 1200) {
+    console.log("Medium screen: " + screenWidth + "px width");
+  } else {
+    console.log("Large screen: " + screenWidth + "px width");
+  }
+}
+
+// Call the function when the page loads and on window resize
+window.onload = window.onresize = checkScreenSize;
